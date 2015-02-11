58eaefd0b8fea0cae8d6039618e8be6b956849be
Michal Novotny <michal.novotny@trustport.com>
2015-02-11 21:42:09 +0100

    Fix

--- create.sh
+++ create.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -f htop.tgz
+
 sourcedir="$(rpm --eval "%{_sourcedir}")"
 name="$(cat htop.spec  | grep Name: | awk '{split($0, a, ":"); print a[2]}' | tr -d ' ')"
 ver="$(cat htop.spec  | grep Version: | awk '{split($0, a, ":"); print a[2]}' | tr -d ' ')"
@@ -22,6 +24,8 @@ do
 	mv $file rpms
 	echo "Package saved as rpms/$(basename $file) ..."
 done
+
+rm -f htop.tgz
 
 echo
 echo "All done"
